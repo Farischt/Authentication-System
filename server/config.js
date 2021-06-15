ad116a275819a28d45337a16b5f35b8434d9f7b8
Faris Chtatou <faris.manage@gmail.com>
2021-06-15 10:01:38 +0200

    env variable for db pass

--- server/config.js
+++ server/config.js
@@ -2,7 +2,7 @@ export const DATABASE_CREDENTIALS = {
   dialect: "postgres",
   host: "localhost",
   username: "faris",
-  password: "lolilollpb",
+  password: process.env.DB_PASSWORD,
   database: "authentication",
   port: 8080,
 }
