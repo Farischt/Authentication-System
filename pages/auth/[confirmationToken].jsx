884f1ed65a303c2addbf077c3b425c6cb4019219
Faris Chtatou <faris.manage@gmail.com>
2021-09-18 14:27:00 +0200

    account confirmation page functional

---
+++ pages/auth/[confirmationToken].jsx
@@ -0,0 +1,67 @@
+import Link from "next/link"
+
+export default function AccountConfirmationPage({ email, first_name }) {
+  return (
+    <>
+      <h1> Your account is verified ! </h1>
+      <p>
+        {" "}
+        Hey {first_name} ! You're email {email} has been verified, you're
+        account is ready ! Next step :{" "}
+        <Link href="/auth">
+          <a> login </a>
+        </Link>{" "}
+      </p>
+    </>
+  )
+}
+
+import Backend from "@/server/index"
+import Database from "@/server/database"
+
+export const getServerSideProps = async (context) => {
+  if (await Backend.getAuthenticatedUser(context)) {
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
+    }
+  }
+
+  const { confirmationToken } = context.params
+  const validToken =
+    confirmationToken &&
+    (await Database.AccountConfirmationToken.findByPk(confirmationToken))
+
+  if (!validToken) {
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
+    }
+  }
+
+  const user = await Database.User.get(validToken.user_id)
+
+  if (!user || user.verified) {
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
+    }
+  }
+  user.verified = true
+  await user.save()
+  // await validToken.destroy()
+
+  return {
+    props: {
+      success: true,
+      email: user.email,
+      first_name: user.first_name,
+    },
+  }
+}
