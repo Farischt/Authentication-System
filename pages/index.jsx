8f6032c95dd6668f5a68a9cdefd571226795e016
Faris Chtatou <faris.manage@gmail.com>
2021-09-20 11:57:51 +0200

    code refacto

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,49 +1,12 @@
-import { useState } from "react"
-import { useRouter } from "next/router"
-import Link from "next/link"
-
-import AuthApi from "@/client/Auth"
+import Layout from "@/components/layout"
 
 export default function Home({ user }) {
-  const router = useRouter()
-  const [loading, setLoading] = useState(false)
-
-  const handleLogout = async (event) => {
-    event.preventDefault()
-    setLoading(true)
-    try {
-      await AuthApi.logout()
-      setLoading(false)
-      router.replace(router.asPath)
-    } catch (error) {
-      console.log(error.message)
-      setLoading(false)
-    }
-  }
-
   return (
-    <div>
-      <h1> Fullstack Auth System</h1>
-      {!user && (
-        <nav>
-          <Link href="/auth/register">
-            <a> register </a>
-          </Link>
-          <br /> {/* to be deleted */}
-          <Link href="/auth">
-            <a> login </a>
-          </Link>
-        </nav>
-      )}
-      {user && (
-        <>
-          <p> Connected as {user.first_name} </p>{" "}
-          <button onClick={handleLogout} disabled={loading}>
-            {loading ? "Loading..." : "Log out"}
-          </button>
-        </>
-      )}
-    </div>
+    <Layout user={user}>
+      <div>
+        <h1> Fullstack Auth System</h1>
+      </div>
+    </Layout>
   )
 }
 
