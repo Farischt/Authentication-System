7ce016540b10f3e0dc0fd194978135e3025f4219
Faris Chtatou <faris.manage@gmail.com>
2021-09-30 01:44:05 +0200

    home page style

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,10 +1,45 @@
 import Layout from "@/components/layout"
+import Link from "next/link"
+
+import styles from "@/styles/pages/index.module.scss"
 
 export default function Home({ user }) {
   return (
     <Layout user={user}>
-      <div>
-        <h1> Fullstack Auth System</h1>
+      <div className={styles.container}>
+        <div className={styles.content}>
+          <h1 className={styles.title}> Welcome to AuthSys </h1>
+          <p className={styles.text}>
+            A FullStack authentication application made from scratch by{" "}
+            <strong>
+              {" "}
+              <a
+                className={styles.link}
+                href="https://github.com/Farischt"
+                target="_blank"
+              >
+                {" "}
+                Farischt{" "}
+              </a>{" "}
+            </strong>
+          </p>
+          <p className={styles.text}>
+            <a
+              className={styles.link}
+              href="https://github.com/Farischt/Authentication-System"
+              target="_blank"
+            >
+              Documentation here !
+            </a>
+          </p>
+          <Link href="/auth" passhref>
+            <button
+              className={`${styles.btn} ${styles.btn__primary} ${styles.center}`}
+            >
+              Get Started !
+            </button>
+          </Link>
+        </div>
       </div>
     </Layout>
   )
